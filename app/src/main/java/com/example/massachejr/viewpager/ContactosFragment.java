797b244b9d7cb0d13b2f32c2772a48b570216146
massache <amassache@gmail.com>
2019-05-07 00:38:25 +0200

    ViewPager Basico

--- app/src/main/java/com/example/massachejr/viewpager/ContactosFragment.java
+++ app/src/main/java/com/example/massachejr/viewpager/ContactosFragment.java
@@ -1,19 +1,30 @@
 package com.example.massachejr.viewpager;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ContactosFragment extends Fragment {
+
+    RecyclerView recyclerView;
+    UserListAdapter adapter;
+
+    List<User> userList;
 
 
     public ContactosFragment() {
@@ -25,7 +36,44 @@ public class ContactosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contactos, container, false);
+        View view = inflater.inflate(R.layout.fragment_contactos, container, false);
+
+        userList = new ArrayList<>();
+        recyclerView = view.findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+
+        userList.add(
+                new User("12345","Angelo","asdasd@c.com","default"));
+        userList.add(
+                new User("12345","Elton","asdasd@k.com","default"));
+        userList.add(
+                new User("122345","Ivan","aodasd@c.com","default"));
+        userList.add(
+                new User("12343","Cedrik","asdayd@c.com","default"));
+        userList.add(
+                new User("12325","Ticongolo","asppd@c.com","default"));
+        userList.add(
+                new User("12345","Gerson","aslksd@c.com","default"));
+        userList.add(
+                new User("12345","Faiane","aslasdad@c.com","default"));
+        userList.add(
+                new User("12345","Josue","aslooood@c.com","default"));
+        userList.add(
+                new User("12345","Mandava","aslksdppp@c.com","default"));
+        userList.add(
+                new User("12345","Socrates","aslksasdasd@c.com","default"));
+        userList.add(
+                new User("12345","Gimo","aslksasdasd@c.com","default"));
+        userList.add(
+                new User("12345","William","aslksasdasd@c.com","default"));
+
+
+        adapter = new UserListAdapter(this.getContext(),userList);
+        recyclerView.setAdapter(adapter);
+
+        
+        return view;
     }
 
 }
